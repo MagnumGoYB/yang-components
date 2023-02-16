6b392d91cdea0af45452bee3d18165c08c2d197c
MagnumGOYB <sosbs@outlook.com>
2023-02-16 17:36:23 +0800

    feat: avatar

---
+++ src/components/Avatar/Avatar.tsx
@@ -0,0 +1,62 @@
+import classNames from 'classnames'
+import { FC } from 'react'
+
+import { NativeProps } from '@/utils'
+
+type AvatarProps = {
+  src?: string
+  alt?: string
+  size?: 'sm' | 'md' | 'lg' | 'xl'
+  shape?: 'circle' | 'square'
+  onClick?: () => void
+}
+
+const Avatar: FC<AvatarProps & NativeProps> = (props) => {
+  const {
+    src,
+    alt,
+    size = 'md',
+    shape = 'circle',
+    onClick,
+    className,
+    style
+  } = props
+
+  const isCustomColor = className?.match(/bg-|text-/)?.length
+
+  return (
+    <div
+      className={classNames('avatar', { placeholder: !src })}
+      onClick={onClick}
+    >
+      <div
+        className={classNames(className, {
+          'bg-neutral-focus text-neutral-content': !src && !isCustomColor,
+          'w-8 h-8': size === 'sm',
+          'w-12 h-12': size === 'md',
+          'w-16 h-16': size === 'lg',
+          'w-24 h-24': size === 'xl',
+          rounded: shape === 'square',
+          'rounded-full': shape === 'circle'
+        })}
+        style={style}
+      >
+        {src ? (
+          <img alt={alt} src={src} />
+        ) : (
+          <span
+            className={classNames({
+              'text-xs': size === 'sm',
+              'text-xl': size === 'lg',
+              'text-3xl': size === 'xl'
+            })}
+          >
+            {alt?.slice(0, 1).toUpperCase()}
+          </span>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default Avatar
